de92754d16f987565f4fbee4c0cf1d6828c48fa1
Joep de Jong <joep@joepdejong.com>
2023-06-03 10:29:18 +0200

    Cleanup code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use axum::{routing::get, Router};
 use groups::{groups_handler, groups_handler_as_array};
 mod groups;
-
-use token::token_handler;
 mod token;
 
 mod models;
@@ -17,8 +15,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/groups/:member_email", get(groups_handler))
-        .route("/groups/:member_email/slugs", get(groups_handler_as_array))
-        .route("/token", get(token_handler));
+        .route("/groups/:member_email/slugs", get(groups_handler_as_array));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
--- src/token.rs
+++ src/token.rs
@@ -1,7 +1,4 @@
-use crate::models::Claims;
 use google_cloud_auth::project::{create_token_source, Config};
-use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
-use serde_json;
 
 pub async fn get_token() -> Result<google_cloud_auth::token::Token, Box<dyn std::error::Error>> {
     let scopes = ["https://www.googleapis.com/auth/cloud-identity.groups.readonly"];
@@ -20,47 +17,4 @@ pub async fn get_token() -> Result<google_cloud_auth::token::Token, Box<dyn std:
     let token = ts.token().await?;
 
     Ok(token)
-}
-
-pub async fn validate_jwt(iap_jwt: String, expected_audience: String) {
-    // This URL contains a JSON dictionary that contains a map from the kid claims to the ES256 public keys
-    let certs_url = "https://www.gstatic.com/iap/verify/public_key";
-    // Fetch the certs from the URL
-    let certs = reqwest::get(certs_url)
-        .await
-        .unwrap()
-        .json::<serde_json::Value>()
-        .await
-        .unwrap();
-
-    // Get the decoding keys
-    let decodingKeys = certs
-        .as_array()
-        .unwrap()
-        .iter()
-        .map(|key| DecodingKey::from_ec_pem(&[key]))
-        .collect::<Vec<DecodingKey>>();
-
-    // Set the expected issuer and audience
-    let issuer = "https://cloud.google.com/iap";
-
-    // Validate the JWT x509
-    let mut validation = Validation::new(Algorithm::ES256);
-    validation.set_audience(&[expected_audience]);
-    validation.set_issuer(&[issuer]);
-
-    let token = match decode::<Claims>(&iap_jwt, &certs, &validation) {
-        Ok(token) => token,
-        Err(e) => {
-            println!("Error: {:?}", e);
-            Ok(())
-        }
-    }
-    println!("Token: {:?}", token.claims);
-    println!("Token: {:?}", token.header);
-}
-
-pub async fn token_handler() {
-    let token = get_token().await.unwrap();
-    println!("token: {:?}", token);
 }
